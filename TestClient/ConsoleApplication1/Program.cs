d4ec927868fdd780fc860908d95d66cf82f7ff5a
V-vltru <iv.vovatrunov__37@mail.ru>
2017-11-09 13:54:16 +0300

    Настроил хостинг службы и добавил тестовый клиент

---
+++ TestClient/ConsoleApplication1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using ConsoleApplication1.ServiceReference1;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputForTemp input = new InputForTemp();
+            
+            //InputForTemp
+            input.C = 1.0;
+            input.H = 1;
+            input.Tau = 0.1;
+            input.TimeSteps = 3;
+
+            double [,]U1 = new double[4,4] { { 0, 0, 0, 0 }, { 1, 0, 0, 0 }, { 2, 0, 0, 0 }, { 3, 0, 0, 0 } };
+            input.U = Utils.ToJagged(U1);
+            
+            Service1Client serv = new Service1Client();
+            OutputForTemp out1 = serv.CalculateTemp(input);
+            //serv.CalculateTemp()
+                
+        }
+    }
+}
