f9c46e2364b0c8ade12bc317de5e5f1fdc11f3e8
AlekseevaT <alisa2667@mail.ru>
2018-05-15 02:07:16 +0300

    Исправлены ошибки в методах решения интегралов, добавлены юнит-тесты

---
+++ TempCalulatorWCF146M/UnitTestSolveIntegralLib/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestSolveIntegralLib")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestSolveIntegralLib")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("745c2d4e-a834-41a4-8c8d-4763bb338fbd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TempCalulatorWCF146M/UnitTestSolveIntegralLib/UnitTest1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SolveIntegralLib;
+
+namespace UnitTestSolveIntegralLib
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestSeqRM()
+        {
+            SeqMeth sm = new SeqMeth();
+            double a = 1;
+            double b = 10000;
+            int N = 10000000;
+            double ans = sm.RectangleMedium(a, b, N, (x) => x * x);
+            double expected = 333333333333;
+            Assert.AreEqual(expected, ans, 1);
+        }
+
+        [TestMethod]
+        public void TestSeqTrapeze()
+        {
+            SeqMeth sm = new SeqMeth();
+            double a = 1;
+            double b = 10000;
+            int N = 10000000;
+            double ans = sm.Trapeze(a, b, N, (x) => x * x);
+            double expected = 333333333333;
+            Assert.AreEqual(expected, ans, 1);
+        }
+
+        [TestMethod]
+        public void TestSeqSimpson()
+        {
+            
+            SeqMeth sm = new SeqMeth();
+            double a = 1;
+            double b = 10000;
+            int N = 10000000;
+            double ans = sm.Simpson(a, b, N, (x) => x * x);
+            double expected = 333333333333;
+            Assert.AreEqual(expected, ans, 1);
+        }
+
+        [TestMethod]
+        public void TestParalRm()
+        {
+            ParallelMets pm = new ParallelMets();
+            double a = 1;
+            double b = 10000;
+            int N = 10000000;
+            double ans = pm.RectangleMedium(a, b, N, (x) => x * x);
+            double expected = 333333333333;
+            Assert.AreEqual(expected, ans, 1);
+        }
+
+        [TestMethod]
+        public void TestParalTrapeze()
+        {
+            ParallelMets pm = new ParallelMets();
+            double a = 1;
+            double b = 10000;
+            int N = 10000000;
+            double ans = pm.Trapeze(a, b, N, (x) => x * x);
+            double expected = 333333333333;
+            Assert.AreEqual(expected, ans, 1);
+        }
+
+        [TestMethod]
+        public void TestParalSimpson()
+        {
+            ParallelMets pm = new ParallelMets();
+            double a = 1;
+            double b = 10000;
+            int N = 100000000;
+            double ans = pm.Simpson(a, b, N, (x) => x * x);
+            double expected = 333333333333;
+            Assert.AreEqual(expected, ans, 1);
+        }
+    }
+}
